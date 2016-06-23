57e0cd9314b5d217a09a0b2d3115fadb412dc191
Mike Harms <harmsm@gmail.com>
2016-06-23 06:31:26 -0700

    ability to fix parameters, some sanity checking

--- README.md
+++ README.md
@@ -4,21 +4,19 @@ A python software package for analyzing Isothermal Titration Calorimetry data
 
 See the Documentation.ipynb jupyter notebook for full descriptions of models.
 
+
 ##Quick Start
 ```Python
 import pytc
 
 # Set up the global fit with parameter guesses
-fitter = pytc.GlobalFit({"K1":1e6,"dH1":-2000,"fx1":1.0,"dil1":0.0})
+fitter = pytc.GlobalFit({"K":1e6,"dH":-2000,"fx_competent":1.0,"dilution_heat":0.0})
 
-# Load in an experiment, defining input data and fitting model
+# Load experimental data, defining input data and fitting model
 e1 = pytc.ITCExperiment("test-data/hA5A5conTESCaTCEP.DH",pytc.models.SingleSite)
 
-# Associate the global K1 parameter with the local K parameter, etc.
-fitter.add_experiment(e1,{"K1":"K",
-                          "dH1":"dH",
-                          "fx1":"fx_competent",
-                          "dil1":"dilution_heat"})
+# Associate the model with the fit
+fitter.add_experiment(e1)
 
 # Do the global fit
 fitter.fit()
@@ -29,3 +27,14 @@ fitter.plot()
 # Print out the final fit parameters
 print(fitter.fit_param)
 ```
+
+###Notes
+
+####Fixing fit parameters
+
+Only parameters passed as `param_guesses` to `GlobalFit` will be fit to the data.
+
+Some fit parameters have natural default values that are specified in the dQ method
+for each model.  For example, the `fx_competent` parameter is a nuisance parameter
+that accounts for a mismatch between the intended and actual concentrations of
+species in the titrant cell.  The default is to fix this value at 1.0
--- pytc/fitting.py
+++ pytc/fitting.py
@@ -8,7 +8,7 @@ experiments.
 __author__ = "Michael J. Harms"
 __date__ = "2016-06-22"
 
-import copy, inspect
+import copy, inspect, warnings
 import numpy as np
 import scipy.optimize as optimize
 from matplotlib import pyplot as plt
@@ -19,7 +19,7 @@ class GlobalFit:
     ITC experiments.
     """
 
-    def __init__(self,param_guesses):
+    def __init__(self,param_guesses,fixed_param=None):
         """
         Set up the main binding model to fit.
         param_guesses: a dictionary of all parameters to be fit.  keys are parameter
@@ -29,6 +29,11 @@ class GlobalFit:
         # Dictionary with all parameters to be fit as keys and initial guesses
         # for their values.
         self._param_guesses = copy.copy(param_guesses)
+
+        # Fixed parameters
+        self._fixed_global_param = {}
+        if fixed_param != None:
+            self._fixed_global_param = copy.copy(fixed_param)
 
         # maps for converting between parameter name and index of parameter in 
         # the np.array used for the fitting.
@@ -47,9 +52,9 @@ class GlobalFit:
         self._experiment_list = []
         self._arg_map_list = []
         self._exp_weights = []
-        self._fixed_param = []
+        self._fixed_local_param = []
 
-    def add_experiment(self,experiment,arg_map=None,weight=1.0,**fixed_params):
+    def add_experiment(self,experiment,arg_map=None,weight=1.0,**fixed_param_kwargs):
         """
         experiment: an initialized ITCExperiment instance
         arg_map: dictionary mapping names of global parameter to names of 
@@ -80,25 +85,56 @@ class GlobalFit:
 
         # Fixed parameter values can be passed as keywords to add_experiment.  If
         # any are present, record them.
-        self._fixed_param.append({}) 
-        if len(fixed_params) != 0:
-            for k in fixed_params.keys():
-                self._fixed_param[-1][k] = fixed_params[k]
+        self._fixed_local_param.append({}) 
+        if len(fixed_param_kwargs) != 0:
+            for k in fixed_param_kwargs.keys():
+                self._fixed_local_param[-1][k] = fixed_param_kwargs[k]
                 
         # Make sure that the global parameter values in arg_map are actually found in
         # the set of global parameters, and that the local parameter names are
         # correct.
         for a in self._arg_map_list[-1].keys():
+
             try:
                 self._name_to_index[a]
             except KeyError:
                 err = "parameter {} not defined in global fit\n".format(a)
                 raise ValueError(err)
 
-            
             if self._arg_map_list[-1][a] not in possible_model_args:
                 err = "the fitting model does not have an argument named {}\n".format(self._arg_map_list[-1][a])
                 raise ValueError(err)
+
+    def _call_dq(self,i,param):
+        """
+        Call the dQ method for experiment i, mapping global parameters to local
+        parameters and fixing specified parameters.
+        """
+
+        # This uses the names of the total model parameters as keys to 
+        # map to the appropriate parameter values being fit to the 
+        # names of those parameters in the local experiment model.
+        model_param_kwarg = {}
+        for a in self._arg_map_list[i].keys():
+            dq_model_param_name = self._arg_map_list[i][a]
+
+            # See if the parameter is globally fixed.  If so, use the fixed
+            # value.  If not, use the param value from the method call
+            try:
+                param_value = self._fixed_global_param[dq_model_param_name]
+            except KeyError:
+                param_value = param[self._name_to_index[a]]
+
+            model_param_kwarg[dq_model_param_name] = param_value
+
+        # A parameter fixed for this local experiment overrides either the 
+        # floating parameter or the globally fixed parameters
+        for a in self._fixed_local_param[i].keys():
+            model_param_kwarg[a] = self._fixed_local_param[i][a]
+
+        # calculate the current value of the model
+        return self._experiment_list[i].model_dq(**model_param_kwarg)
+
 
     def _residuals(self,param):
         """
@@ -110,21 +146,8 @@ class GlobalFit:
         # Calculate residuals for each experiment
         for i, e in enumerate(self._experiment_list):
 
-            # This uses the names of the total model parameters as keys to 
-            # map to the appropriate parameter values being fit to the 
-            # names of those parameters in the local experiment model.
-            model_param_kwarg = {}
-            for a in self._arg_map_list[i].keys():
-                dq_model_param_name = self._arg_map_list[i][a]
-                param_value = param[self._name_to_index[a]]
-                model_param_kwarg[dq_model_param_name] = param_value
-
-            for a in self._fixed_param[i].keys():
-                model_param_kwarg[a] = self._fixed_param[i][a]
-
-            # calculate the current value of the model
-            calc = e.model_dq(**model_param_kwarg)
-            
+            calc = self._call_dq(i,param)
+ 
             # add to the total set of residuals
             all_residuals.extend(self._exp_weights[i]*(e.heats - calc))
 
@@ -135,6 +158,23 @@ class GlobalFit:
         """
         Perform a global fit using nonlinear regression.
         """
+
+        # Some sanity checking
+        if len(self._experiment_list) == 0:
+            warn = "No experiments loaded.  Fit is trivial.\n"
+            warnings.warn(warn)
+        
+        if len(self._experiment_list) > 1:
+            for i in range(len(self._experiment_list)):
+                if len(self._fixed_local_param[i]) != 0:
+                    warn = "Individual experiment(s) have fixed parameters.  These will be ignored by the global fit for this experiment, but could float for other experiments in the data set.  To fix a parameter globally, pass it to GlobalFit in the fixed_param dictionary.  See documentation for details."
+                    warnings.warn(warn)
+
+                    # Get out of loop so the warning only appears once.
+                    break
+
+        # Set starting value for fit
+        self._fit_param = self._fit_param_array
         
         # Do the actual fit
         fit_param, covariance, info_dict, message, error = optimize.leastsq(self._residuals,
@@ -148,7 +188,16 @@ class GlobalFit:
         """
         Plot the experimental data and fit results.
         """
-        
+       
+        # Only make a plot if the fit has already been run 
+        try:
+            self._fit_param
+        except AttributeError:
+            warn = "Fit has not yet been run.  No plot generated."
+            warnings.warn(warn)
+            return      
+ 
+ 
         if color_list == None:
             N = len(self._experiment_list)
             color_list = [plt.cm.brg(i/N) for i in range(N)]
@@ -159,14 +208,10 @@ class GlobalFit:
         
         for i, e in enumerate(self._experiment_list):
             
-            model_param_kwarg = {}
-            for a in self._arg_map_list[i].keys():
-                dq_param_name = self._arg_map_list[i][a]
-                param_value = self.fit_param[a]
-                model_param_kwarg[dq_param_name] = param_value
-            
+            calc = self._call_dq(i,self._fit_param)
+ 
             plt.plot(e.mole_ratio,e.heats,"o",color=color_list[i])
-            plt.plot(e.mole_ratio,e.model_dq(**model_param_kwarg),color=color_list[i],linewidth=1.5)
+            plt.plot(e.mole_ratio,calc,color=color_list[i],linewidth=1.5)
         
     
     @property
@@ -174,10 +219,19 @@ class GlobalFit:
         """
         Return the fit results as a dictionary that keys parameter name to fit value.
         """
+       
+        try:
+ 
+            out_dict = {}
+            for i, a in enumerate(self._index_to_name):
+                out_dict[a] = self._fit_param[i]
+       
+            for a in self._fixed_global_param.keys():
+                out_dict[a] = self._fixed_global_param[a]
         
-        out_dict = {}
-        for i, a in enumerate(self._index_to_name):
-            out_dict[a] = self._fit_param[i]
-        
+        # If the fit has not been run, return an empty dictionary    
+        except AttributeError:
+            out_dict = {}
+
         return out_dict
         
