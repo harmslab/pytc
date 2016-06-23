9dfe6d6d9f8d0127cb1c0cfe46fc38a766a1d4f3
Mike Harms <harmsm@gmail.com>
2016-06-23 04:25:27 -0700

    added integrated heat-of-dilution fit, cleaned up

--- pytc/models.py
+++ pytc/models.py
@@ -25,7 +25,7 @@ class ITCModel:
     @property
     def mole_ratio(self):
         
-        return self._A_total[1:]/self._P_total[1:]
+        return self._T_conc[1:]/self._S_conc[1:]
 
 class SingleSite(ITCModel):
     """
@@ -33,26 +33,26 @@ class SingleSite(ITCModel):
     """
     
     def __init__(self,
-                 P_cell=100e-6,P_syringe=0.0,
-                 A_cell=0.0,   A_syringe=1000e-6,
+                 S_cell=100e-6,S_syringe=0.0,
+                 T_cell=0.0,   T_syringe=1000e-6,
                  cell_volume=300.0,
                  shot_volumes=[2.5 for i in range(30)]):
                  
         """
-        P_cell: protein concentration in cell in M
-        P_syringe: protein concentration in syringe in M
-        A_cell: ligand A concentration cell in M
-        A_syringe: ligand A concentration syringe in M
+        S_cell: stationary concentration in cell in M
+        S_syringe: stationary concentration in syringe in M
+        T_cell: titrant concentration cell in M
+        T_syringe: titrant concentration syringe in M
         cell_volume: cell volume, in uL
         shot_volumes: list of shot volumes, in uL. 
         shot_start: first shot to use in fit
         """
         
-        self._P_cell = P_cell
-        self._P_syringe = P_syringe
+        self._S_cell = S_cell
+        self._S_syringe = S_syringe
         
-        self._A_cell = A_cell
-        self._A_syringe = A_syringe
+        self._T_cell = T_cell
+        self._T_syringe = T_syringe
             
         self._cell_volume = cell_volume
         self._shot_volumes = np.array(shot_volumes)
@@ -62,18 +62,18 @@ class SingleSite(ITCModel):
     
     def _determine_titration_conc(self):
         """
-        Determine the total concentrations of P and A in tn the cell given 
-        a set of titration shots and initial concentrations of P and A in
-        the cell and syringe.
+        Determine the concentrations of stationary and titrant species in the
+        cell given a set of titration shots and initial concentrations of both 
+        the stationary and titrant species. 
         """
         
-        self._volume  = np.zeros(len(self._shot_volumes)+1)
-        self._P_total = np.zeros(len(self._shot_volumes)+1)
-        self._A_total = np.zeros(len(self._shot_volumes)+1)
+        self._volume = np.zeros(len(self._shot_volumes)+1)
+        self._S_conc = np.zeros(len(self._shot_volumes)+1)
+        self._T_conc = np.zeros(len(self._shot_volumes)+1)
         
-        self._volume[0]  = self._cell_volume
-        self._P_total[0] = self._P_cell
-        self._A_total[0] = self._A_cell
+        self._volume[0] = self._cell_volume
+        self._S_conc[0] = self._S_cell
+        self._T_conc[0] = self._T_cell
         
         for i in range(len(self._shot_volumes)):
             
@@ -82,27 +82,27 @@ class SingleSite(ITCModel):
             dilution = self._volume[i]/self._volume[i+1]
             added = self._shot_volumes[i]/self._volume[i+1]
             
-            self._P_total[i+1] = self._P_total[i]*dilution + self._P_syringe*added
-            self._A_total[i+1] = self._A_total[i]*dilution + self._A_syringe*added
+            self._S_conc[i+1] = self._S_conc[i]*dilution + self._S_syringe*added
+            self._T_conc[i+1] = self._T_conc[i]*dilution + self._T_syringe*added
             
-    def dQ(self,KA,dHA,fx_comp=1.0):
+    def dQ(self,K,dH,fx_competent=1.0,dilution_heat=0.0):
         """
-        Calculate the heats that would be observed across shots for a given set of enthalpies
-        and binding constants for each reaction.
+        Calculate the heats that would be observed across shots for a given set
+        of enthalpies and binding constants for each reaction.
         """
         
         # ----- Determine mole fractions -----
-        P = self._P_total*fx_comp
-        b = P + self._A_total + 1/KA
-        PA = (b - np.sqrt((b)**2 - 4*P*self._A_total))/2
+        S_conc_corr = self._S_conc*fx_competent
+        b = S_conc_corr + self._T_conc + 1/K
+        ST = (b - np.sqrt((b)**2 - 4*S_conc_corr*self._T_conc))/2
     
-        self._x_pa = PA/P
-        self._x_p = 1 - self._x_pa
+        self._mol_fx_st = ST/S_conc_corr
+        self._mol_fx_s = 1 - self._mol_fx_st
         
         # ---- Relate mole fractions to heat -----
-        A = dHA*(self._x_pa[1:] - self._x_pa[:-1])
+        X = dH*(self._mol_fx_st[1:] - self._mol_fx_st[:-1])
     
-        to_return = self._cell_volume*(self._P_total[1:]*fx_comp)*A
+        to_return = self._cell_volume*S_conc_corr[1:]*X + self._T_conc[1:]*dilution_heat
         
         return to_return
 
@@ -116,55 +116,54 @@ class SingleSiteCompetitor(ITCModel):
     """
  
     def __init__(self,            
-                 P_cell=100e-6,P_syringe=0.0,
-                 A_cell=0.0,   A_syringe=1000e-6,
-                 B_cell=200e-6,B_syringe=0.0,
+                 S_cell=100e-6,S_syringe=0.0,
+                 T_cell=0.0,   T_syringe=1000e-6,
+                 C_cell=200e-6,C_syringe=0.0,
                  cell_volume=300.0,
                  shot_volumes=[2.5 for i in range(30)]):
                  
         """
-        P_cell: protein concentration in cell in M
-        P_syringe: protein concentration in syringe in M
-        A_cell: ligand A concentration cell in M
-        A_syringe: ligand A concentration syringe in M
-        B_cell: ligand B concentration cell in M
-        B_syringe: ligand B concentration syringe in M
+        S_cell: stationary concentration in cell in M
+        S_syringe: stationary concentration in syringe in M
+        T_cell: titrant concentration cell in M
+        T_syringe: titrant concentration syringe in M
+        C_cell: competitor concentration cell in M
+        C_syringe: competitor concentration syringe in M
         cell_volume: cell volume, in uL
         shot_volumes: list of shot volumes, in uL. 
         shot_start: first shot to use in fit
         """
         
-        self._P_cell = P_cell
-        self._P_syringe = P_syringe
+        self._S_cell = S_cell
+        self._S_syringe = S_syringe
          
-        self._A_cell = A_cell
-        self._A_syringe = A_syringe
+        self._T_cell = T_cell
+        self._T_syringe = T_syringe
         
-        self._B_cell = B_cell
-        self._B_syringe = B_syringe
+        self._C_cell = C_cell
+        self._C_syringe = C_syringe
         
         self._cell_volume = cell_volume
         self._shot_volumes = np.array(shot_volumes)
         
         self._determine_titration_conc()
                  
-                
     def _determine_titration_conc(self):
         """
-        Determine the total concentrations of P, A, and B in the cell given 
-        a set of titration shots and initial concentrations of P, A and B in
-        the cell and syringe.
+        Determine the concentrations of stationary, titrant and competitor 
+        species in the cell given a set of titration shots and initial
+        concentrations of species in the cell and syringe.
         """
         
-        self._volume  = np.zeros(len(self._shot_volumes)+1)
-        self._P_total = np.zeros(len(self._shot_volumes)+1)
-        self._A_total = np.zeros(len(self._shot_volumes)+1)
-        self._B_total = np.zeros(len(self._shot_volumes)+1)
+        self._volume = np.zeros(len(self._shot_volumes)+1)
+        self._S_conc = np.zeros(len(self._shot_volumes)+1)
+        self._T_conc = np.zeros(len(self._shot_volumes)+1)
+        self._C_conc = np.zeros(len(self._shot_volumes)+1)
         
-        self._volume[0]  = self._cell_volume
-        self._P_total[0] = self._P_cell
-        self._A_total[0] = self._A_cell
-        self._B_total[0] = self._B_cell
+        self._volume[0] = self._cell_volume
+        self._S_conc[0] = self._S_cell
+        self._T_conc[0] = self._T_cell
+        self._C_conc[0] = self._C_cell
         
         for i in range(len(self._shot_volumes)):
             
@@ -173,39 +172,39 @@ class SingleSiteCompetitor(ITCModel):
             dilution = self._volume[i]/self._volume[i+1]
             added = self._shot_volumes[i]/self._volume[i+1]
             
-            self._P_total[i+1] = self._P_total[i]*dilution + self._P_syringe*added
-            self._A_total[i+1] = self._A_total[i]*dilution + self._A_syringe*added
-            self._B_total[i+1] = self._B_total[i]*dilution + self._B_syringe*added
+            self._S_conc[i+1] = self._S_conc[i]*dilution + self._S_syringe*added
+            self._T_conc[i+1] = self._T_conc[i]*dilution + self._T_syringe*added
+            self._C_conc[i+1] = self._C_conc[i]*dilution + self._C_syringe*added
 
 
-    def dQ(self,KA,KB,dHA,dHB,fx_comp=1.0):
+    def dQ(self,K,Kcompetitor,dH,dHcompetitor,fx_competent=1.0,dilution_heat=0.0):
         """
-        Calculate the heats that would be observed across shots for a given set of enthalpies
-        and binding constants for each reaction.
+        Calculate the heats that would be observed across shots for a given set
+        of enthalpies and binding constants for each reaction.
         """
     
         # ----- Determine mole fractions -----
-        P = self._P_total*fx_comp
+        S_conc_corr = self._S_conc*fx_competent
     
-        c_a = KA*P
-        c_b = KB*P
-        r_a = self._A_total/(P)
-        r_b = self._B_total/(P)
+        c_a = K*S_conc_corr
+        c_b = Kcompetitor*S_conc_corr
+        r_a = self._T_conc/S_conc_corr
+        r_b = self._C_conc/S_conc_corr
         
         alpha = 1/c_a + 1/c_b + r_a + r_b - 1
         beta = (r_a - 1)/c_b + (r_b - 1)/c_a + 1/(c_a*c_b)
         gamma = -1/(c_a*c_b)
         theta = np.arccos((-2*alpha**3 + 9*alpha*beta - 27*gamma)/(2*np.sqrt((alpha**2 - 3*beta)**3)))
     
-        self._x_p = (2*np.sqrt(alpha**2 - 3*beta) * np.cos(theta/3) - alpha)/3
-        self._x_pa = r_a*self._x_p/(1/c_a + self._x_p)
-        self._x_pb = r_b*self._x_p/(1/c_b + self._x_p)
+        self._mol_fx_s = (2*np.sqrt(alpha**2 - 3*beta) * np.cos(theta/3) - alpha)/3
+        self._mol_fx_st = r_a*self._mol_fx_s/(1/c_a + self._mol_fx_s)
+        self._mol_fx_sc = r_b*self._mol_fx_s/(1/c_b + self._mol_fx_s)
     
         # ---- Relate mole fractions to heat -----
-        A = dHA*(self._x_pa[1:] - self._x_pa[:-1])
-        B = dHB*(self._x_pb[1:] - self._x_pb[:-1])
+        X = dH*(self._mol_fx_st[1:] - self._mol_fx_st[:-1])
+        Y = dHcompetitor*(self._mol_fx_sc[1:] - self._mol_fx_sc[:-1])
         
-        to_return = self._cell_volume*P*(A + B)
+        to_return = self._cell_volume*S_conc_corr[1:]*(X + Y) + self.S_conc[1:]*dilution_heat
         
         return to_return
         
