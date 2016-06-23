5112afecda9fec053a16dfb9f1ec3033a9792c8b
Mike Harms <harmsm@gmail.com>
2016-06-22 17:53:55 -0700

    initial commit

---
+++ pytc/__init__.py
@@ -0,0 +1,15 @@
+__description__ = \
+"""
+Python package for performing fits of arbitrarily complex thermodynamic models
+to isothermal titration calorimetry experiments.
+"""
+__author__ = "Michael J. Harms"
+__date__ = "2016-06-22"
+__all__ = ["experiments","models","fitting"]
+
+from . import experiments
+from . import models
+from . import fitting
+
+from .experiments import ITCExperiment
+from .fitting import GlobalFit
