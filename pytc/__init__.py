655c32c9c99f45bca751805ddc461ffebcb45289
Mike Harms <harmsm@gmail.com>
2017-01-11 12:45:23 -0800

    updated README and init

--- pytc/__init__.py
+++ pytc/__init__.py
@@ -10,6 +10,7 @@ __all__ = ["experiments","indiv_models","global_models","util"]
 from . import experiments
 from . import indiv_models
 from . import global_models
+from . import util
 
 # Load these defaults (which will be used for vast majority of experiments) 
 # into the global namespace for convenience
